e515b9846b5ce8ff2e011f9a2335bb912d7d0d38
m0slah <muhammedslahqadir@gmail.com>
2024-04-01 10:55:19 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { sum } from '@/components/useSum';
+
+const firstNumber = ref(0);
+const secondNumber = ref(0);
+const total = computed(() => sum(firstNumber.value, secondNumber.value));
+</script>
+
+<template>
+  <div>Hello! The sum is: {{ total }}
+    <input v-model.number="firstNumber" type="number" />
+    <input v-model.number="secondNumber" type="number" />
+  </div>
+</template>
---
+++ src/components/useSum.ts
@@ -0,0 +1,4 @@
+// useSum.ts
+export const sum = (a: number, b: number): number => {
+    return a + b;
+};
